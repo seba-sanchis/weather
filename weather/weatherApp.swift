fd159e704f79d172fed1fcb8fa260d2c0d18c850
seba-sanchis <seba.sanchis@gmail.com>
2023-11-15 19:15:25 -0300

    Initial Commit

---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Sebastian Sanchis on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
